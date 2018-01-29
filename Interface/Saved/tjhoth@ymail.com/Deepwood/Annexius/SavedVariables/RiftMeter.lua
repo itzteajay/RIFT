f5b82a31caf7b6798413ac720ba78965c5c42b57
itzteajay <itzteajay@users.noreply.github.com>
2018-01-28 21:02:58 -0800

    Initial commit

---
+++ Interface/Saved/tjhoth@ymail.com/Deepwood/Annexius/SavedVariables/RiftMeter.lua
@@ -0,0 +1,145 @@
+RiftMeter_abbreviateNumbers = true
+RiftMeter_abilityColors = {
+	air = {
+		1,
+		1,
+		1
+	},
+	death = {
+		0.81,
+		0.31,
+		1
+	},
+	earth = {
+		1,
+		1,
+		0
+	},
+	fire = {
+		1,
+		0,
+		0
+	},
+	life = {
+		0,
+		1,
+		0
+	},
+	physical = {
+		0.6,
+		0.6,
+		0.6
+	},
+	water = {
+		0,
+		0.5,
+		1
+	}
+}
+RiftMeter_absorbAsDamage = true
+RiftMeter_alwaysShowPlayer = true
+RiftMeter_classColors = {
+	cleric = {
+		0.46667,
+		0.93725,
+		0
+	},
+	mage = {
+		0.78431,
+		0.36863,
+		1
+	},
+	none = {
+		1,
+		1,
+		1
+	},
+	primalist = {
+		0.231,
+		0.842,
+		1
+	},
+	rogue = {
+		1,
+		0.85882,
+		0
+	},
+	warrior = {
+		1,
+		0.15686,
+		0.15686
+	}
+}
+RiftMeter_customHighlightBackgroundColor = false
+RiftMeter_customHighlightBackgroundColorValue = {
+	0,
+	0,
+	0
+}
+RiftMeter_customHighlightFontColor = false
+RiftMeter_customHighlightFontColorValue = {
+	1,
+	1,
+	0
+}
+RiftMeter_lock = false
+RiftMeter_mergeAbilitiesByName = true
+RiftMeter_mergePets = true
+RiftMeter_showAbsolute = true
+RiftMeter_showPercent = true
+RiftMeter_showRankNumber = true
+RiftMeter_showScrollbar = false
+RiftMeter_showTooltips = true
+RiftMeter_totalbarColor = {
+	0,
+	0.5,
+	1
+}
+RiftMeter_windows = {
+	{
+		backgroundColor = {
+			0,
+			0,
+			0,
+			0.5
+		},
+		footerBackgroundColor = {
+			0,
+			0,
+			0,
+			0.7
+		},
+		hideButtons = {
+		},
+		rowFontColor = {
+			1,
+			1,
+			1
+		},
+		rowFontSize = 12,
+		rowHeight = 18,
+		rowPadding = 1,
+		rowTexture = "textures\\BantoBar.dds",
+		rows = 8,
+		showEnemies = false,
+		sort = "damage",
+		swapTextAndBarColor = false,
+		titlebarColor = {
+			0,
+			0,
+			0,
+			1
+		},
+		titlebarFontColor = {
+			1,
+			1,
+			1
+		},
+		titlebarFontSize = 12,
+		titlebarHeight = 24,
+		titlebarTexture = "textures\\header.png",
+		width = 280,
+		x = 1655,
+		y = 862
+	}
+}
---
+++ guild.xslt
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+<xsl:template match="/">
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Toasty Crew Info</title>
+</head>
+    <body>
+        <h1>Toasty Crew Info</h1>
+        <table style="width:100%;">
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    </body>
+</html>
+</xsl:template>
+</xsl:stylesheet>
